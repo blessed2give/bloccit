d285c19fae84d7dfe7c75ddcd0413c0831f2a2d6
Paul <paucho1437@yahoo.com>
2017-03-06 13:42:15 -0600

    rails assignment 24 authentication attempt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase if email.present? }
-
+  before_save {
+    name_arr = self.name.split(" ")
+    name_arr.each do |i|
+      i.capitalize!
+    end
+    name_arr.join(" ")
+  }
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
 
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
