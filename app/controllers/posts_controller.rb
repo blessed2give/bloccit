49b5e819265e8a026676245157541627cbaf0a65
Paul <paucho1437@yahoo.com>
2017-03-08 14:09:49 -0600

    rails cp 27 posts and users completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :require_sign_in, except: :show
+
   def show
     @post = Post.find(params[:id])
   end
@@ -9,25 +11,21 @@ class PostsController < ApplicationController
   end
 
   def create
-# #9
-  @post = Post.new
-  @post.title = params[:post][:title]
-  @post.body = params[:post][:body]
-  @topic = Topic.find(params[:topic_id])
- # #35
-  @post.topic = @topic
+    @topic = Topic.find(params[:topic_id])
+    @post = @topic.posts.build(post_params)
+    @post.user = current_user
 
-# #10
-  if @post.save
-# #11
-    flash[:notice] = "Post was saved."
-    redirect_to [@topic, @post]
-  else
-# #12
-    flash.now[:alert] = "There was an error saving the post. Please try again."
-    render :new
+  # #10
+    if @post.save
+  # #11
+      flash[:notice] = "Post was saved."
+      redirect_to [@topic, @post]
+    else
+  # #12
+      flash.now[:alert] = "There was an error saving the post. Please try again."
+      render :new
+    end
   end
-end
 
   def edit
     @post = Post.find(params[:id])
@@ -35,8 +33,7 @@ end
 
   def update
     @post = Post.find(params[:id])
-    @post.title = params[:post][:title]
-    @post.body = params[:post][:body]
+    @post.assign_attributes(post_params)
 
     if @post.save
       flash[:notice] = "Post was updated."
@@ -58,6 +55,11 @@ end
       flash.now[:alert] = "There was an error deleting the post."
       render :show
     end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 
 end
