9aabe19b70136500d62c4e5c67d02e2919f6c090
Paul <paucho1437@yahoo.com>
2017-03-09 15:57:04 -0600

    rails assignment 28 authorization attempted

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,6 +1,7 @@
 class TopicsController < ApplicationController
   before_action :require_sign_in, except: [:index, :show]
   before_action :authorize_user, except: [:index, :show]
+  before_action :moderator, except: [:index, :show]
 
   def index
     @topics = Topic.all
@@ -63,6 +64,13 @@ class TopicsController < ApplicationController
   def authorize_user
     unless current_user.admin?
       flash[:alert] = "You must be an admin to do that."
+      redirect_to topics_path
+    end
+  end
+
+  def moderator
+    unless current_user.moderator?
+      flash[:alert] = "You must be a moderator to do that."
       redirect_to topics_path
     end
   end
