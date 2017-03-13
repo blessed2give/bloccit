0d668355471e34c64b3b24b295eab537f1cc7165
Paul <paucho1437@yahoo.com>
2017-03-13 11:12:51 -0500

    rails assignment 32 public profiles draft

---
+++ spec/factories/votes.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :post do
+    value 0
+    user
+    post
+  end
+end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Vote, type: :model do
   let(:topic) { create(:topic) }
   let(:user) { create(:user) }
   let(:post) { create(:post) }
-  let(:vote) { Vote.create!(value: 1, post: post, user: user) }
+  let(:vote) { create(:vote) }
 
   it { is_expected.to belong_to(:post) }
   it { is_expected.to belong_to(:user) }
@@ -12,6 +12,8 @@ RSpec.describe Vote, type: :model do
   it { is_expected.to validate_inclusion_of(:value).in_array([-1, 1]) }
 
   describe "update_post callback" do
+    let(:vote) { create(:vote, value: 1) }
+    
     it "triggers update_post on save" do
       expect(vote).to receive(:update_post).at_least(:once)
       vote.save!
